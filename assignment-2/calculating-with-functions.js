0a395cac17e9c89679a940da8662651473f41018
Divya <divya.gandivalasa@gmail.com>
2017-08-03 23:49:34 +0530

    review comments

--- assignment-2/calculating-with-functions.js
+++ assignment-2/calculating-with-functions.js
@@ -1,58 +1,37 @@
-function calculation(num, func) {
-  if (func === undefined) {
-    return num;
-  } else {
-    return func(num);
+function calculate(num) {
+  return function (func) {
+    return func ? func(num) : num;
   }
 }
-function zero(func) {
-  return calculation(0, func);
-}
-function one(func) {
-  return calculation(1, func);
-}
-function two(func) {
-  return calculation(2, func);
-}
-function three(func) {
-  return calculation(3, func);
-}
-function four(func) {
-  return calculation(4, func);
-}
-function five(func) {
-  return calculation(5, func);
-}
-function six(func) {
-  return calculation(6, func);
-}
-function seven(func) {
-  return calculation(7, func);
-}
-function eight(func) {
-  return calculation(8, func);
-}
-function nine(func) {
-  return calculation(9, func);
-}
+
+var zero = calculate(0);
+var one = calculate(1);
+var two = calculate(2);
+var three = calculate(3);
+var four = calculate(4);
+var five = calculate(5);
+var six = calculate(6);
+var seven = calculate(7);
+var eight = calculate(8);
+var nine = calculate(9);
 
 function plus(rightOperand) {
   return function (leftOperand) {
-    return rightOperand + leftOperand;
+    return leftOperand + rightOperand;
   };
 }
 function minus(rightOperand) {
   return function (leftOperand) {
-    return rightOperand - leftOperand;
+    return leftOperand - rightOperand;
   };
 }
 function times(rightOperand) {
   return function (leftOperand) {
-    return rightOperand * leftOperand;
+    return leftOperand * rightOperand;
   };
 }
 function dividedBy(rightOperand) {
   return function (leftOperand) {
-    return rightOperand / leftOperand;
+    return leftOperand / rightOperand;
   };
 }
--- assignment-2/get-the-middle-character.js
+++ assignment-2/get-the-middle-character.js
@@ -1,8 +1,9 @@
 function getMiddle(inputWord) {
-  var centerIndex = inputWord.length / 2;
+  var result, centerIndex = inputWord.length / 2;
   if (inputWord.length % 2 == 0) {
-    return inputWord.charAt(centerIndex - 1) + inputWord.charAt(centerIndex);
+    result = inputWord.charAt(centerIndex - 1) + inputWord.charAt(centerIndex);
   } else {
-    return inputWord.charAt(centerIndex);
+    result = inputWord.charAt(centerIndex);
   }
+  return result;
 }
